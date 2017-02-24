b20cfbb11c71a6bad6633da1f10f8691e739abb4
ucha19871 <ucha19871@gmail.com>
2017-02-24 15:47:43 +0400

    computed property

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -13,9 +13,19 @@
         <button :title="buttonTitle">Hover over</button>
 
         <h1 :class="className">Something </h1>
-
         <button :class="{'isLoading':isLoading}" @click="toggleClass">Cllllick</button>
+        <hr>
+        <h3>{{reverseMessage}}</h3>
+        <hr>
+        <h3>Tasks</h3>
+        <ul>
+            <li v-for="task in tasks">{{task.title}} <button :disabled="task.completed">Complete Task</button></li>
+        </ul>
 
+        <h3>inCompleted Tasks</h3>
+        <ul>
+            <li v-for="itask in incompleteTasks">{{itask.title}} </li>
+        </ul>
 
     </div>
 </template>
@@ -26,14 +36,21 @@
         data () {
             return {
                 isLoading: false,
-                className:'color-red',
+                className: 'color-red',
                 buttonTitle: 'text to show  up on hovering on the button',
-                newName:'',
-                names: ['Ucha', 'Tucha', 'Dgucha', 'Bucha', 'Nucha', 'Sucha', 'Klucha', 'Tynucha']
+                newName: '',
+                names: ['Ucha', 'Tucha', 'Dgucha', 'Bucha', 'Nucha', 'Sucha', 'Klucha', 'Tynucha'],
+                tasks:[
+                    {title:'Go to store',completed:true},
+                    {title:'Finish coding',completed:false},
+                    {title:'Clean table',completed:true},
+                    {title:'Go to Bank',completed:false},
+                    {title:'Clear inbox',completed:true},
+                ]
             }
         },
 
-        methods:{
+        methods: {
             addName(){
                 this.names.push(this.newName)
                 this.newName = '';
@@ -44,6 +61,15 @@
                 this.isLoading = !this.isLoading;
             }
 
+        },
+
+        computed: {
+            reverseMessage(){
+                return this.buttonTitle.split('').reverse().join('');
+            },
+            incompleteTasks(){
+                return this.tasks.filter(task => !task.completed);
+            }
         }
 
 
@@ -54,13 +80,16 @@
     .test {
         text-align: left;
     }
-    .color-red{
-        color:red ;
+
+    .color-red {
+        color: red;
     }
-    .color-blue{
-        color:blue ;
+
+    .color-blue {
+        color: blue;
     }
-    .isLoading{
+
+    .isLoading {
         background: red;
     }
 </style>
