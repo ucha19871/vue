0d41724a2874160803261c64e972edbd0ff3889f
ucha19871 <ucha19871@gmail.com>
2017-02-26 20:21:21 +0400

    install form validator

--- src/components/Test2.vue
+++ src/components/Test2.vue
@@ -11,7 +11,29 @@
     </el-col>
   </el-row>
     
-    
+    <el-row>
+      <table class="table">
+        <thead>
+          <tr>
+            <th><abbr title="ID">ID</abbr></th>
+            <th><abbr title="Title">Title</abbr></th>
+            <th><abbr title="Body">Body</abbr></th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr v-for="post in posts">
+            <th>{{post.id}}</th>
+            <td>{{post.title}}</td>
+            <td>{{post.body}}</td>
+            
+          </tr>
+          
+        </tbody>
+      </table>
+      
+
+    </el-row>
 
 
   </div>
@@ -24,19 +46,40 @@ export default {
   name: 'test2',
   data () {
     return {
+      posts: [],
       textApplied: false,
       msg: 'Welcome to Your Vue Project'
     }    
   },
+
+
+
 
   methods:{
 
       testUpdated(){
         this.textApplied = true;
         this.$emit('applied')
+      },
+
+      getPosts(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => this.posts = res.data )
+        .catch(error => console.log(error));
       }
 
+
+    },
+
+    computed: {
+    pageCount: function () {
+      return this.posts.length
     }
+  },
+
+  created(){
+    this.getPosts();
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import Axios from 'axios'
 import 'element-ui/lib/theme-default/index.css'
 
+
 Vue.use(ElementUI)
+Vue.prototype.$http = Axios
 
 /* eslint-disable no-new */
 new Vue({
